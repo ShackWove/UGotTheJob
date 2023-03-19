7169362be55eb55c5ac31cb1340e1667fb9a87d4
Giu57 <76096146+Giu57@users.noreply.github.com>
2023-03-20 00:07:51 +0100

    + modified Model class

--- source_code/business_logic/Model.py
+++ source_code/business_logic/Model.py
@@ -1,13 +1,13 @@
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 from joblib import load
 
 class Model:
     def __init__(self):
-        self.dt : DecisionTreeClassifier() = load ("job_prevision.joblib")
+        self.rf : RandomForestClassifier() = load ("job_prevision.joblib")
     
     def prediction_model(self, dataframe: pd):
-        prediction = self.dt.predict(dataframe)
+        prediction = self.rf.predict(dataframe)
         if prediction[0] == "Not Placed":
             return "Non sarai preso"
         elif prediction[0] == "Placed":
