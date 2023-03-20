69bf27869c64d5ca2bf4ce419a6de6d7a273e1eb
Giu57 <76096146+Giu57@users.noreply.github.com>
2023-03-20 23:41:51 +0100

    + modified classes and jupyter file

--- source_code/business_logic/Data.py
+++ source_code/business_logic/Data.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 class Data:
 
-    def prepare_data(self, hsc_percentage = 37, hsc_subject = "Science", degree_percentage = 50 , undergrad_degree ="Sci&Tech", work_experience="No", emp_test_percentage = 50, specialisation = "Mkt&HR"):
+    def prepare_data(self, hsc_percentage = 37, hsc_subject = "Science", degree_percentage = 50 , undergrad_degree ="Sci&Tech", work_experience="No",  specialisation = "Mkt&HR"):
         degree_percentage_converted = degree_percentage - 10
         return pd.DataFrame({"hsc_percentage": [hsc_percentage], "hsc_subject": [hsc_subject], 
                                                 "degree_percentage": [degree_percentage_converted], "undergrad_degree" : [undergrad_degree], 
-                                                "work_experience": [work_experience], "emp_test_percentage": [emp_test_percentage], 
+                                                "work_experience": [work_experience], 
                                                 "specialisation": [specialisation]})
 
 
--- source_code/business_logic/Model.py
+++ source_code/business_logic/Model.py
@@ -4,11 +4,9 @@ from joblib import load
 
 class Model:
     def __init__(self):
-        self.rf : RandomForestClassifier() = load ("job_prevision.joblib")
+        self.dt : RandomForestClassifier() = load ("job_prevision.joblib")
     
     def prediction_model(self, dataframe: pd):
-        prediction = self.rf.predict(dataframe)
-        if prediction[0] == "Not Placed":
-            return "Non sarai preso"
-        elif prediction[0] == "Placed":
-            return "Sarai preso"
+        prediction = self.dt.predict(dataframe)
+        prob = self.dt.predict_proba(dataframe)[:, 1]
+        return "Non sarai preso al " + str(prob[0] * 100) + "%", "Sarai preso al "+ str(100 - (prob[0] *100)) +"%"
