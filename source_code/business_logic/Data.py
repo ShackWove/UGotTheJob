f64402fd908591cf7a0b9c17e308ca5b9ad52bb8
Giu57 <76096146+Giu57@users.noreply.github.com>
2023-03-20 18:20:15 +0100

    + modified files

--- source_code/business_logic/Data.py
+++ source_code/business_logic/Data.py
@@ -4,13 +4,11 @@ import pandas as pd
 class Data:
 
     def prepare_data(self, hsc_percentage = 37, hsc_subject = "Science", degree_percentage = 50 , undergrad_degree ="Sci&Tech", work_experience="No", emp_test_percentage = 50, specialisation = "Mkt&HR"):
-        degree_percentage_converted = Data.undegree_percentage_convert(degree_percentage)
+        degree_percentage_converted = degree_percentage - 10
         return pd.DataFrame({"hsc_percentage": [hsc_percentage], "hsc_subject": [hsc_subject], 
                                                 "degree_percentage": [degree_percentage_converted], "undergrad_degree" : [undergrad_degree], 
                                                 "work_experience": [work_experience], "emp_test_percentage": [emp_test_percentage], 
                                                 "specialisation": [specialisation]})
 
-    @classmethod
-    def undegree_percentage_convert(cls, degree_percentage):
-        return degree_percentage - 10
+
 
