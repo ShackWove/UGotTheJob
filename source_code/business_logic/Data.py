84e733836ca449b235eaa9f4dd55d27d9a67f0b6
Giu57 <76096146+Giu57@users.noreply.github.com>
2023-03-22 12:56:10 +0100

    + fixed bugs

--- source_code/business_logic/Data.py
+++ source_code/business_logic/Data.py
@@ -3,10 +3,9 @@ import pandas as pd
 
 class Data:
 
-    def prepare_data(self, hsc_percentage = 37, hsc_subject = "Science", degree_percentage = 50 , undergrad_degree ="Sci&Tech", work_experience="No",  specialisation = "Mkt&HR"):
-        degree_percentage_converted = degree_percentage - 10
-        return pd.DataFrame({"hsc_percentage": [hsc_percentage], "hsc_subject": [hsc_subject], 
-                                                "degree_percentage": [degree_percentage_converted], "undergrad_degree" : [undergrad_degree], 
+    def prepare_data(self,ssc_percentage=50, ssc_board= 0,  hsc_percentage = 40, hsc_board=0,  hsc_subject = 0, degree_percentage = 50 , undergrad_degree =0, work_experience=0,  specialisation =0):
+        return pd.DataFrame({"ssc_percentage": [ssc_percentage], "ssc_board": [ssc_board],"hsc_percentage": [hsc_percentage], "hsc_board" : hsc_board,"hsc_subject": [hsc_subject], 
+                                                "degree_percentage": [degree_percentage], "undergrad_degree" : [undergrad_degree], 
                                                 "work_experience": [work_experience], 
                                                 "specialisation": [specialisation]})
 
--- source_code/business_logic/Model.py
+++ source_code/business_logic/Model.py
@@ -8,8 +8,10 @@ class Model:
     
     def prediction_model(self, dataframe: pd):
         prediction = self.dt.predict(dataframe)
-        prob = self.dt.predict_proba(dataframe)[:, 1]
+        
         if prediction[0] == "Placed" :
-            return "Sarai preso al " + str(prob[0] * 100) + "%", "Non sarai preso al "+ str(100 - (prob[0] *100)) +"%"
+            prob = self.dt.predict_proba(dataframe)[:, 1]
+            return "Sarai preso al " + str(prob[0] * 100)
         else:
-            return "Sarai preso al " + str(100 - (prob[0] * 100)) + "%", "Non sarai preso al "+ str(prob[0] *100) +"%"
+            prob = self.dt.predict_proba(dataframe)[:, 0]
+            return "Non sarai preso al " + str(prob[0] * 100) 
